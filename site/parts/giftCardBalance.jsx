bcd639870e2b7fb62c5c0b38cc54f12a076fe7ed
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:30 +0330

    committed by script

--- site/parts/giftCardBalance.jsx
+++ site/parts/giftCardBalance.jsx
@@ -1,4 +1,4 @@
-export default ({ giftCard }) => <dl class='giftCardBalance'>
+export default ({ giftCard }) => <dl class='balance'>
     <dt class='code'>{giftCard.code}</dt>
     <dd class='balance'>{giftCard.balance}</dd>
     <dd class='currency'>{giftCard.currency?.title}</dd>
